cde4d500679770bf52bb26a4c3ecf177686f64aa
Gustavo <rosalesagustavo@gmail.com>
2022-06-13 15:46:18 -0400

    installed bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("bootstrap")
+  }, []);
+
   return <Component {...pageProps} />
 }
 
