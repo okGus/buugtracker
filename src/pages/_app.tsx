308c4b6750bfe8c457bd50f202977c6ec8759fb7
Gustavo <rosalesagustavo@gmail.com>
2022-07-17 14:39:15 -0400

    starting with trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
-import { ReactElement, ReactNode, useEffect } from 'react'
+import { useEffect } from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
-import { NextPage } from 'next'
-
-// function Layout({children}: {children: ReactElement}) {
-//   return <></>
-// }
-
-// type NextPageWithLayout = NextPage & {
-//   getLayout?: (page: ReactElement) => ReactNode;
-// }
-
-// type AppPropsWithLayout = AppProps & {
-//   Component: NextPageWithLayout;
-// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     import("bootstrap")
   }, []);
-
-  //const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
     <UserProvider>
@@ -31,4 +16,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+import { withTRPC } from '@trpc/next';
+import type { AppRouter } from '../backend/router'
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
