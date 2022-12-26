cca223c429ed17f7312acd42928ea83f5fa4bb93
Gustavo <rosalesagustavo@gmail.com>
2022-12-26 18:24:10 -0500

    Nextjs-cors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,23 @@
-import * as trpcNext from '@trpc/server/adapters/next';
+// import * as trpcNext from '@trpc/server/adapters/next';
 import { appRouter } from '../../../backend/router';
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import cors from "nextjs-cors";
 
-// export API handler
-export default trpcNext.createNextApiHandler({
+// // export API handler
+// export default trpcNext.createNextApiHandler({
+//     router: appRouter,
+//     createContext: () => null,
+//   });
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res);
+
+  return createNextApiHandler({
     router: appRouter,
     createContext: () => null,
-  });
+  })(req, res);
+};
+
+export default handler;
